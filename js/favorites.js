93f7023e0084fec6490b3a602a0a997a1af363bd
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-03-12 15:51:21 -0700

    beginnging to add more functional buttons

--- js/favorites.js
+++ js/favorites.js
@@ -1,5 +1,4 @@
 let favoriteCountDowns = JSON.parse(localStorage.getItem('favorites'));
-console.log(favoriteCountDowns)
 
 let favoriteBtns = []
 
@@ -8,8 +7,19 @@ for (let i = 0; i < favoriteCountDowns.length; i++) {
   if (favoriteCountDowns[i].songName.includes('music.mp3/teamWork.mp3')) favoriteCountDowns[i].songName = "Team Work"
   if (favoriteCountDowns[i].songName.includes('music.mp3/dragonForce.mp3')) favoriteCountDowns[i].songName = "Dragon Force"
 
-  let newBtn = '<button>' + favoriteCountDowns[i].songName + " Second: " + favoriteCountDowns[i].sec + " Minute: " + favoriteCountDowns[i].min + " Hour: " + favoriteCountDowns[i].hour + "</button>" + "<br>";
+  let newBtn = '<button id="newBtn">' + '<span>'+ favoriteCountDowns[i].songName + " Second: " + favoriteCountDowns[i].sec + " Minute: " + favoriteCountDowns[i].min + " Hour: " + favoriteCountDowns[i].hour + '</span>' + "</button>" + "<br>";
 
   favoriteBtns.push(newBtn)
   document.querySelector("#demo").innerHTML = favoriteBtns;
+}
+
+
+
+
+//how to link favorite buttons to the new countdown page
+
+
+
+newBtn.onclick = function() {
+  alert("it works")
 }
